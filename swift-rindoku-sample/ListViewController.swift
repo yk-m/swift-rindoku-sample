de0d2b3d5c8b82a82812168f873415935e9b8865
hicka04 <calpissoda8221@gmail.com>
2018-08-11 11:31:40 +0900

    add project

---
+++ swift-rindoku-sample/ListViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  swift-rindoku-sample
+//
+//  Created by hicka04 on 2018/08/11.
+//  Copyright © 2018 hicka04. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    @IBOutlet private weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
