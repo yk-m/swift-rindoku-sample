f87944f88ed0ce3103927c53263741306ce25e5d
Yuka Matsuo <matsuo.yuka@nifty.co.jp>
2018-09-27 11:59:11 +0900

    複数のセル、複数のセクションを表示する

--- swift-rindoku-sample/ListViewController.swift
+++ swift-rindoku-sample/ListViewController.swift
@@ -11,9 +11,15 @@ import UIKit
 class ListViewController: UIViewController {
     
     @IBOutlet private weak var tableView: UITableView!
+    let items = ["a", "b", "c", "d"]
+    let sections = ["1", "2", "3"]
+    
+    let cellId = "cell"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
         
         tableView.delegate = self
         tableView.dataSource = self
@@ -22,13 +28,21 @@ class ListViewController: UIViewController {
 
 // MARK: - UITableViewDelegate, UITableViewDataSource
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return items.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
-        return cell
+        let newCell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        newCell.textLabel?.text = items[indexPath.row]
+        return newCell
     }
 }
